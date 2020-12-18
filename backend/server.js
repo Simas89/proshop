7770a4930c520fd4f5509554afa7d23953dd69dd
Simas89 <simaszurauskas@gmail.com>
2020-12-18 17:54:29 +0000

    ok

--- backend/server.js
+++ backend/server.js
@@ -35,18 +35,18 @@ app.get('/api/config/paypal', (req, res) =>
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
 
+	// app.get('*', (req, res) =>
+	// 	res.sendFile(
+	// 		path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+	// 	)
+	// );
+
+	app.use(express.static('../client/build'));
 	app.get('*', (req, res) =>
 		res.sendFile(
 			path.resolve(__dirname, '../', 'client', 'build', 'index.html')
 		)
 	);
-
-	// app.use(express.static('../client/build'));
-	// app.get('*', (req, res) =>
-	//   res.sendFile(
-	//     path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-	//   )
-	// );
 }
 
 app.use(notFound);
