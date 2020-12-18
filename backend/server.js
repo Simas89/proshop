c076eff80bc2f4af4447ce41695729d9149b7ff3
Simas89 <simaszurauskas@gmail.com>
2020-12-18 18:07:25 +0000

    ok

--- backend/server.js
+++ backend/server.js
@@ -35,18 +35,18 @@ app.get('/api/config/paypal', (req, res) =>
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
 
-	// app.get('*', (req, res) =>
-	// 	res.sendFile(
-	// 		path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-	// 	)
-	// );
-
-	app.use(express.static('../client/build'));
 	app.get('*', (req, res) =>
 		res.sendFile(
 			path.resolve(__dirname, '../', 'client', 'build', 'index.html')
 		)
 	);
+
+	// app.use(express.static('../client/build'));
+	// app.get('*', (req, res) =>
+	//   res.sendFile(
+	//     path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+	//   )
+	// );
 }
 
 app.use(notFound);
@@ -55,4 +55,4 @@ app.use(errorHandler);
 /// ---
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, console.log(`Server running on port: ${PORT}`.cyan));
+app.listen(PORT, console.log(`Server running on port: ${PORT}`.cyan));
