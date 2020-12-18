0ec64d7a3ff600ea74d4cc7278bec80a6572a5ae
Simas89 <simaszurauskas@gmail.com>
2020-12-18 19:33:53 +0000

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ import colors from 'colors';
 import morgan from 'morgan';
 import connectDB from './config/db';
 import { notFound, errorHandler } from './middleware/errorMiddleware';
-// import productRoutes from './routes/productRoutes';
-// import userRoutes from './routes/userRoutes';
-// import orderRoutes from './routes/orderRoutes';
-// import uploadRoutes from './routes/uploadRoutes';
+import productRoutes from './routes/productRoutes';
+import userRoutes from './routes/userRoutes';
+import orderRoutes from './routes/orderRoutes';
+import uploadRoutes from './routes/uploadRoutes';
 
 dotenv.config();
 connectDB();
@@ -18,6 +18,7 @@ const app = express();
 /// ---
 
 console.log('Hello World!');
+console.log(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,10 +26,10 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(morgan('dev'));
 
-// app.use('/api/products', productRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/orders', orderRoutes);
-// app.use('/api/upload', uploadRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
